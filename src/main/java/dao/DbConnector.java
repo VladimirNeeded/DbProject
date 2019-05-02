86739502aa578ea78490edfdfbd8187386eb5611
VladimirNeeded <voha.tretyak@gmail.com>
2019-05-02 11:23:12 +0300

    hw14 (done)

--- src/main/java/dao/DbConnector.java
+++ src/main/java/dao/DbConnector.java
@@ -2,19 +2,20 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Optional;
 
 public class DbConnector {
     private static final String dbUrl = "jdbc:mysql://localhost:3306/mate_academy?useSSL=false";
     private static final String name = "root";
     private static final String password = "4815162342";
 
-    public static Connection connect() {
+    public static Optional<Connection> connect() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            return DriverManager.getConnection(dbUrl, name, password);
+            return Optional.of(DriverManager.getConnection(dbUrl, name, password));
         }catch (Exception e){
             e.printStackTrace();
         }
-        return null;
+        return Optional.empty();
     }
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -5,14 +5,16 @@ import model.User;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import java.util.Optional;
 
 public class UserDao {
 
     public void addUser(User user) {
-        Connection connection = DbConnector.connect();
+
+        Optional<Connection> connection =  DbConnector.connect();
+
         try {
-            Statement statement = connection.createStatement();
+            Statement statement = connection.get().createStatement();
             String sqlAdd = "INSERT INTO `mate_academy`.`users` (`login`, `password`) VALUES ('" + user.getLogin() + "', '" + user.getPassword() + "');";
             System.out.println(sqlAdd);
             statement.execute(sqlAdd);
