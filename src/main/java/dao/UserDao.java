6293d1e320312db7824017ecf924e7f0884d573d
VladimirNeeded <voha.tretyak@gmail.com>
2019-05-02 19:30:16 +0300

    hw14 (done2)

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -3,16 +3,17 @@ package dao;
 import model.User;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Optional;
 
 public class UserDao {
 
+    Optional<Connection> connection =  DbConnector.connect();
+
     public void addUser(User user) {
-
         Optional<Connection> connection =  DbConnector.connect();
-
         try {
             Statement statement = connection.get().createStatement();
             String sqlAdd = "INSERT INTO `mate_academy`.`users` (`login`, `password`) VALUES ('" + user.getLogin() + "', '" + user.getPassword() + "');";
@@ -21,5 +22,20 @@ public class UserDao {
         } catch (SQLException e) {
             System.out.println("SQLException//////////////////////////////////");
         }
+    }
+
+    public Optional<String> selectPassword(String login) {
+        try {
+            Statement statement = connection.get().createStatement();
+            String sqlSelect = "SELECT password FROM mate_academy.users WHERE login = '" + login + "';";
+            ResultSet resultPassword = statement.executeQuery(sqlSelect);
+            while (resultPassword.next()) {
+                Optional<String> resPass = Optional.of(resultPassword.getString("password"));
+                return resPass;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
     }
 }
---
+++ src/main/java/servlet/SignInServlet.java
@@ -0,0 +1,37 @@
+package servlet;
+
+import dao.DbConnector;
+import dao.UserDao;
+import model.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.Optional;
+
+@WebServlet(value = "/Sign_In")
+public class SignInServlet extends HttpServlet {
+
+    private static final UserDao userDao = new UserDao();
+    Optional<Connection> connection =  DbConnector.connect();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        User user = new User(login, password);
+        String sqlPassword = userDao.selectPassword(user.getLogin()).get();
+        if (password.equals(sqlPassword)){
+            out.print("Hello, " + login);
+        }else {
+            out.print("Login or password are not valid");
+        }
+    }
+}
